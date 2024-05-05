ffea5572eb0a431707344fb9db3d7b2e9db77510
cepix1234 <bocepek027@gmail.com>
2024-05-05 23:00:57 +0200

    Clean debug output

--- app/cepix1234.WgetTracker.App/obj/Debug/net8.0/win-x64/cepix1234.WgetTracker.App.AssemblyInfo.cs
+++ app/cepix1234.WgetTracker.App/obj/Debug/net8.0/win-x64/cepix1234.WgetTracker.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cepix1234.WgetTracker.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8780d1806e421045eaeaa507f32beea8dc3dca0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cepix1234.WgetTracker.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cepix1234.WgetTracker.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- app/cepix1234.WgetTracker.Infrastructure/obj/Debug/net8.0/cepix1234.WgetTracker.Infrastructure.AssemblyInfo.cs
+++ app/cepix1234.WgetTracker.Infrastructure/obj/Debug/net8.0/cepix1234.WgetTracker.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cepix1234.WgetTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097ff33eb9bf182f4070410e9c1f0cf1ad6b0a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8780d1806e421045eaeaa507f32beea8dc3dca0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cepix1234.WgetTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cepix1234.WgetTracker.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
