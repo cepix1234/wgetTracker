53d988f63dda8e25eb95d40ceb111505cff16878
cepix1234 <bocepek027@gmail.com>
2024-09-01 21:13:56 +0200

    Fix publish.sh for linux

--- publish.sh
+++ publish.sh
@@ -2,33 +2,34 @@ dotnet publish ./app/cepix1234.WgetTracker.App -r linux-x64 -p:PublishSingleFile
 dotnet publish ./app/cepix1234.WgetTracker.App -r win-x64 -p:PublishSingleFile=true --self-contained true
 dotnet publish ./app/cepix1234.WgetTracker.App -r linux-arm64 -p:PublishSingleFile=true --self-contained true
 
-ReleasePath = "./Release"
-ReleaseWinPath = "${ReleasePath}/Windows"
-ReleaseLinuxPath = "${ReleasePath}/Linux"
-ReleaseLinuxArmPath = "${ReleasePath}/LinuxArm"
+ReleasePath="./Release"
+ReleaseWinPath="${ReleasePath}/Windows"
+ReleaseLinuxPath="${ReleasePath}/Linux"
+ReleaseLinuxArmPath="${ReleasePath}/LinuxArm"
 
-if [ ! -d "$ReleasePath"]; then
+if [ ! -d "$ReleasePath" ]; then
   mkdir "$ReleasePath"
   mkdir "$ReleaseWinPath"
   mkdir "$ReleaseLinuxPath"
   mkdir "$ReleaseLinuxArmPath"
 fi
 
-if [ ! -d "$ReleaseWinPath"]; then
+if [ ! -d "$ReleaseWinPath" ]; then
   mkdir "$ReleaseWinPath"
 fi
 
-if [ ! -d "$ReleaseLinuxPath"]; then
+if [ ! -d "$ReleaseLinuxPath" ]; then
   mkdir "$ReleaseLinuxPath"
 fi
 
-if [ ! -d "$ReleaseLinuxArmPath"]; then
+if [ ! -d "$ReleaseLinuxArmPath" ]; then
   mkdir "$ReleaseLinuxArmPath"
 fi
 
 cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/win-x64/publish/cepix1234.WgetTracker.App.exe "${ReleaseWinPath}/cepix1234.WgetTracker.exe"
-cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/win-x64/publish/appsettings.json "${ReleaseWinPath}/appsettings.json"
-cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/linux-x64\publish/cepix1234.WgetTracker.App "${ReleaseLinuxPath}/cepix1234.WgetTracker"
-cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/linux-x64\publish/appsettings.json "${ReleaseLinuxPath}/appsettings.json"
-cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/linux-arm64\publish/cepix1234.WgetTracker.App "${ReleaseLinuxArmPath}/cepix1234.WgetTracker"
-cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/linux-arm64\publish/appsettings.json "${ReleaseLinuxArmPath}/appsettings.json"
+cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/linux-x64/publish/cepix1234.WgetTracker.App "${ReleaseLinuxPath}/cepix1234.WgetTracker"
+cp ./app/cepix1234.WgetTracker.App/bin/Release/net8.0/linux-arm64/publish/cepix1234.WgetTracker.App "${ReleaseLinuxArmPath}/cepix1234.WgetTracker"
+
+cp ./app/cepix1234.WgetTracker.App/appsettings.json "${ReleaseWinPath}/appsettings.json"
+cp ./app/cepix1234.WgetTracker.App/appsettings.json "${ReleaseLinuxPath}/appsettings.json"
+cp ./app/cepix1234.WgetTracker.App/appsettings.json "${ReleaseLinuxArmPath}/appsettings.json"
